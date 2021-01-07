fb69dc67114952a4fb92085e1eb154a4488e1750
fabanc <fabien.ancelin@gmail.com>
2021-01-06 20:37:29 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # arcgis-plus-code-generator
+
+
+# Description
+
+This tool exposes the open location code API create by google and uses it to generate Plus Codes on ArcGIS feature classes. The API documentation is there: https://github.com/google/open-location-code
+
+Detailed information about Plus Codes and the KML Service that Google Provides can be found there: https://grid.plus.codes/
+
+This tool is a side effect of a project I have done for work. I have explored different solutions with FME and QGIS, but this was by far the fastest solution. It can generate plus code on points, and polygon feature classes.
+
+# System Requirements
+
+This tool is developed and tested for ArcGIS Pro. I have tested for ArcGIS Pro 2.7.
+
+Your python environment must be of 3.x (tested with 3.7, but I assume it work with 3.6 too). 
+
+The open location code library for Python must be installed in your python environment. In short
+`pip install openlocationcode`. For detailed instructions: https://github.com/google/open-location-code/tree/master/python
+
+# How to use
+
+Use the tool box in ArcGIS Pro. There is built in help in the tool metadata, and the tool is configured to guide you and minimize input errors.
+
+# How to contribute
+
+This tool has been done in rush. Though is works, it does not match my standards or the industry standards in terms of unit testing or documentation.
+If you want to help, or have any suggestion, log an issue, and a pull request if you feel bold enough.
+
+
+
+
---
+++ scripts/gp_open_location_geocode.py
@@ -0,0 +1,98 @@
+import arcpy
+from openlocationcode import openlocationcode as olc
+
+# https://github.com/google/open-location-code/blob/master/python/openlocationcode_test.py
+
+
+def check_spatial_reference(feature_class):
+    sr = arcpy.Describe(feature_class).spatialReference
+    if sr.factoryCode != 4326:
+        raise ValueError('The input feature class must be in WHS84 (WKID 4326)')
+
+
+def validate_plus_code_length(code):
+
+    # Those are the number of digit required from the specifications.
+    valid_codes_length = [
+        2,  # Level 0
+        4,  # Level 1
+        6,  # Level 2
+        8,  # Level 3
+        10,  # Level 4
+        11,  # Level 5
+        12  # Level 6
+    ]
+
+    if code not in valid_codes_length:
+        raise ValueError('Valid Plus Code must be one of the following value: {}'.format(
+            ', '.join([str(code) for code in valid_codes_length])
+        ))
+
+
+def validate_code_field_length(feature_class, plus_code_field, code_length):
+    fields = [field for field in arcpy.ListFields(feature_class) if field.name.lower() == plus_code_field.lower()]
+    if fields is None or len(fields) == 0:
+        raise Exception('The field {} does not exist'.format(plus_code_field))
+
+    field = fields[0]
+    if field.type != 'String':
+        raise Exception('The field for plus code must be of type String')
+    if field.length < (code_length + 1):
+        raise Exception('The field {} is not long enough. Field Length: {} - Plus Code Length: {} - Plus Code Length Required: {}'.format(
+            field.name,
+            field.length,
+            code_length,
+            code_length + 1
+        ))
+
+
+def generate_plus_code(feature_class, plus_code_field, code_length):
+    """
+    Generate the plus code based on a input feature class. The centroid of the geometry is used.
+
+    :param feature_class: The input feature class. The EPSG must be 4326
+    :param plus_code_field: The field that will contain the plus codes.
+    :param code_length: The maximum length for the plus code.
+    :return:
+    """
+
+    # Make sure that the input feature class is in the appropriate format.
+    check_spatial_reference(feature_class)
+
+    # Validate that the plus code is a valid value
+    validate_plus_code_length(code_length)
+
+    # Validate the field is long enough to accomodate the code.
+    validate_code_field_length(feature_class, plus_code_field, code_length)
+
+    count = 0
+    with arcpy.da.UpdateCursor(feature_class, ['SHAPE@XY', plus_code_field, 'OID@']) as input_cursor:
+        for input_row in input_cursor:
+
+            oid = input_row[-1]
+            count += 1
+
+            if count % 100000 == 0:
+                arcpy.AddMessage('Processed {} rows so far ...'.format(count))
+
+            try:
+                longitude = input_row[0][0]
+                latitude = input_row[0][1]
+                plus_code = olc.encode(latitude, longitude, code_length)
+                input_row[1] = plus_code
+                input_cursor.updateRow(input_row)
+            except Exception as ex:
+                arcpy.AddWarning(
+                    'Something went wrong with feature OID: {} - Error Message: {}'.format(oid, ex.message)
+                )
+
+
+if __name__ == '__main__':
+    input_feature_class = arcpy.GetParameterAsText(0)
+    output_plus_code_field = arcpy.GetParameterAsText(1)
+    output_code_length = int(arcpy.GetParameterAsText(2))
+    try:
+        generate_plus_code(input_feature_class, output_plus_code_field, output_code_length)
+    except Exception as ex:
+        arcpy.AddError(ex)
+
