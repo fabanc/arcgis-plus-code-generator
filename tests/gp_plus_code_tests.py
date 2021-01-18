dd017a435c3c5204cb982bd7ed8b9edb8b4d1af9
fabanc <fabien.ancelin@gmail.com>
2021-01-17 20:05:00 -0500

    Fix #9

--- tests/gp_plus_code_tests.py
+++ tests/gp_plus_code_tests.py
@@ -60,5 +60,75 @@ class ErrorTests(unittest.TestCase):
         feature_class = os.path.join(self.file_database, 'field_bad_type')
         self.assertRaises(custom_errors.FieldTypeError, gp.generate_plus_code, feature_class, 'PLUS_CODE', 4)
 
+
+class ValidTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.schema = os.path.join(
+            CURRENT_FOLDER,
+            'data',
+            'valid_cases_template.xml'
+        )
+
+        database_name = 'ValidCases.gdb'
+        cls.file_database = os.path.join(OUTPUT_FOLDER, database_name)
+        cls.feature_class_polygon = os.path.join(cls.file_database, 'codes_length_poly')
+        cls.feature_class_point = os.path.join(cls.file_database, 'codes_length_point')
+
+        if arcpy.Exists(cls.file_database):
+            arcpy.Delete_management(cls.file_database)
+
+        # Create the file geodatabase
+        arcpy.management.CreateFileGDB(
+            out_folder_path=OUTPUT_FOLDER,
+            out_name=database_name
+        )
+
+        # Import XML template
+        arcpy.management.ImportXMLWorkspaceDocument(
+            cls.file_database,
+            in_file=cls.schema,
+            import_type='DATA'
+        )
+
+    def test_points(self):
+
+        plus_code_8_field = 'Plus_Code_8'
+        plus_code_10_field = 'Plus_Code_10'
+        feature_class = self.feature_class_point
+        gp.generate_plus_code(feature_class, plus_code_8_field, 8)
+        gp.generate_plus_code(feature_class, plus_code_10_field, 10)
+        count = 0
+        with arcpy.da.SearchCursor(feature_class, [plus_code_8_field, plus_code_10_field]) as cursor:
+            for row in cursor:
+                plus_code_8 = str(row[0])
+                plus_code_10 = str(row[1])
+
+                self.assertEqual(len(plus_code_8), 9)
+                self.assertEqual(len(plus_code_10), 11)
+                count += 1
+        self.assertEqual(count, 1)
+
+
+    def test_polygon(self):
+
+        plus_code_8_field = 'Plus_Code_8'
+        plus_code_10_field = 'Plus_Code_10'
+        feature_class = self.feature_class_polygon
+        gp.generate_plus_code(feature_class, plus_code_8_field, 8)
+        gp.generate_plus_code(feature_class, plus_code_10_field, 10)
+        count = 0
+        with arcpy.da.SearchCursor(feature_class, [plus_code_8_field, plus_code_10_field]) as cursor:
+            for row in cursor:
+                plus_code_8 = str(row[0])
+                plus_code_10 = str(row[1])
+
+                self.assertEqual(len(plus_code_8), 9)
+                self.assertEqual(len(plus_code_10), 11)
+                count += 1
+        self.assertEqual(count, 1)
+
+
 if __name__ == '__main__':
     unittest.main()
